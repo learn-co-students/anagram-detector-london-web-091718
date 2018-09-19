6dc9511b2c159b3624997aafb2cb1ccd95ebbe7c
ssong-eu <sangyoon.song@gmail.com>
2018-09-20 00:35:29 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(string)
+    match = []
+    string.each do |word|
+      if word.split("").sort == @anagram.split("").sort
+        match << word
+      end
+    end
+    match
+  end
+
+end
