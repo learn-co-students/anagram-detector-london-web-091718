5af65f25967ccb50fe2441c75625eec26594690c
deedeeh <dinahafez.8@gmail.com>
2018-09-24 10:21:11 +0100

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.select do |word|
+      word if word.split("").sort == @anagram.split("").sort
+    end
+  end
+
+end
+
+p 'eof'
